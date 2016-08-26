80243acf9f740521e0197e79af5d0e40021d3a30
Jess Smith <smith.jessk@gmail.com>
2016-08-26 17:55:07 -0400

    Add custom JSON field names to `common/io_interfaces`

--- common/io_interfaces.go
+++ common/io_interfaces.go
@@ -2,21 +2,36 @@ package common
 
 // AppIDInfoReply is the response to `GET /v1/info/:appID`.
 type AppIDInfoReply struct {
-	appName       string // string specifying displayable app name
-	baseURL       string // string specifying the prefix of all routes
-	appID         string // base64Web encoded appID
-	serverPubKey  string // The server public key. Depends on key type
-	serverKeyType string // The key type. Only P256 supported for now.
+	// string specifying displayable app name
+	AppName string `json:"appName"`
+
+	// string specifying the prefix of all routes
+	BaseURL string `json:"baseURL"`
+
+	// base64Web encoded appID
+	AppID string `json:"appID"`
+
+	// The server public key. Depends on key type
+	ServerPubKey string `json:"serverPubKey"`
+
+	// The key type. Only P256 supported for now.
+	ServerKeyType string `json:"serverKeyType"`
 }
 
 // RegistrationRequestReply is the response to `POST /v1/register/request`.
 type RegistrationRequestReply struct {
-	id          string // base64Web encoded random reply id
-	registerUrl string // Url at which the registration iframe can be found. Pass to frontend.
+	// base64Web encoded random reply id
+	ID string `json:"id"`
+
+	// Url at which the registration iframe can be found. Pass to frontend.
+	RegisterURL string `json:"registerUrl"`
 }
 
 // AuthRequestReply is the response to `POST /v1/auth/request`.
 type AuthRequestReply struct {
-	id      string // base64Web encoded random reply id
-	authUrl string // Url at which the registration iframe can be found. Pass to frontend.
+	// base64Web encoded random reply id
+	ID string `json:"id"`
+
+	// Url at which the registration iframe can be found. Pass to frontend.
+	AuthURL string `json:"authUrl"`
 }
