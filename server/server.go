75d970e7143f4e54850b806ba5d94ed0de7ad37a
Jess Smith <smith.jessk@gmail.com>
2016-08-26 18:02:35 -0400

    Add and test route to get information about app

--- server/server.go
+++ server/server.go
@@ -3,18 +3,37 @@
 package server
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+
+	"2q2r/common"
 )
+
+// Taken from https://git.io/v6xHB
+func writeJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	encoder := json.NewEncoder(w)
+	encErr := encoder.Encode(data)
+	if encErr != nil {
+		log.Printf("Failed to encode data as JSON: %v", encErr)
+	}
+}
 
 func Handler() http.Handler {
 	r := mux.NewRouter()
-	r.HandleFunc("/", http.NotFound)
-	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "world")
+	r.HandleFunc("/v1/info/{appID}", func(w http.ResponseWriter, r *http.Request) {
+		data := new(common.AppIDInfoReply)
+		data.AppName = "foo_app"
+		data.BaseURL = "https://example.com/baz"
+		data.AppID = mux.Vars(r)["appID"]
+		data.ServerPubKey = "Call me beep me"
+		data.ServerKeyType = "P256"
+		writeJSON(w, http.StatusOK, data)
 	})
 	return r
 }
--- server/server_test.go
+++ server/server_test.go
@@ -3,25 +3,29 @@
 package server
 
 import (
-	"io/ioutil"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"2q2r/common"
 )
 
 func TestHello(t *testing.T) {
 	ts := httptest.NewServer(Handler())
 	defer ts.Close()
-	res, err := http.Get(ts.URL + "/hello")
+	appID := "my_id"
+	res, err := http.Get(ts.URL + "/v1/info/" + appID)
 	if err != nil {
 		t.Error(err)
 	}
-	greeting, err := ioutil.ReadAll(res.Body)
+	info := new(common.AppIDInfoReply)
+	err = json.NewDecoder(res.Body).Decode(info)
 	res.Body.Close()
 	if err != nil {
 		t.Error(err)
 	}
-	if string(greeting) != "world" {
-		t.Errorf("Expected response \"world\". Got response %s", string(greeting))
+	if info.AppID != appID {
+		t.Errorf("Expected appID %s. Got response %s", appID, info.AppID)
 	}
 }
