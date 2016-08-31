33927ffebefe97e80e343d40a339eac9375ab627
Jess smith <smith.jessk@gmail.com>
2016-08-31 14:31:03 -0400

    Add `AppServerInfo` model

--- server/models.go
+++ server/models.go
@@ -13,3 +13,25 @@ type AppInfo struct {
 	AuthType string
 	AuthData string // JSON
 }
+
+// AppServerInfo is the Gorm model that holds information about an app server.
+type AppServerInfo struct {
+	gorm.Model
+
+	ServerID string
+
+	// Base URL for users to connect to
+	BaseURL string
+
+	// A server can only serve one app
+	AppID string
+
+	// P256, etc.
+	KeyType string
+
+	// JSON
+	PublicKey string
+
+	// JSON array containing a subset of ["Register", "Delete", "Login"]
+	Permissions string
+}
