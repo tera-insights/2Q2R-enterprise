a91056f164111490fe38c02083c945cc7a45cac4
Jess smith <smith.jessk@gmail.com>
2016-09-13 14:29:57 -0400

    Rename auth_e2e_test.go

--- server/register_e2e_test.go
+++ server/register_e2e_test.go
@@ -5,6 +5,8 @@ package server
 import (
 	"testing"
 	"time"
+
+	"github.com/tstranex/u2f"
 )
 
 // Create app server
@@ -12,7 +14,7 @@ import (
 // Add key to system
 
 func TestIFrameAuthentication(t *testing.T) {
-	serverName := "auth_e2e_test"
+	serverName := "register_e2e_test"
 
 	// Create app server
 	postJSON("/v1/admin/server/new", NewServerRequest{
@@ -51,6 +53,22 @@ func TestIFrameAuthentication(t *testing.T) {
 	// In a separate routine, wait for the registration challenge to be met
 
 	// Sign the challenge and send the result to /v1/register
+	cd := u2f.ClientData{
+		Typ:       "uh",
+		Challenge: string(gleanedData.Challenge),
+		Origin: ts.URL,
+		CIDPubKey: ,
+	}
+	rr := RegisterRequest{
+		Successful: true,
+		Data: successfulRegistrationData{
+			ClientData,       // base64 serialized client data
+			RegistrationData, // base64 binary registration data
+			DeviceName:       "register_e2e_client",
+			Type:             "2Q-2R", // device type and key type
+			FCMToken,         // Firebase Communicator Device token
+		},
+	}
 
 	// Assert that the waiting thread came to a close
 }
