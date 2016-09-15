481e7a0e6be727932a2fd9a25ca23ed263a5facd
Jess smith <smith.jessk@gmail.com>
2016-09-15 13:03:09 -0400

    Change DB location for cmd/server.go

--- cmd/server.go
+++ cmd/server.go
@@ -13,7 +13,7 @@ func main() {
 	c := server.Config{
 		Port:                            ":8080",
 		DatabaseType:                    "sqlite3",
-		DatabaseName:                    "server/test.db",
+		DatabaseName:                    "test.db",
 		ExpirationTime:                  5 * time.Minute,
 		CleanTime:                       30 * time.Second,
 		BaseURL:                         "127.0.0.1",
