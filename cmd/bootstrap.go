1de069223f5afd2cce197c31791ad575cabb554b
Jess smith <smith.jessk@gmail.com>
2016-09-16 17:54:44 -0400

    Change bootstrap process and remove test.db

---
+++ cmd/bootstrap.go
@@ -0,0 +1,39 @@
+// Copyright 2016 Tera Insights, LLC. All Rights Reserved.
+
+package main
+
+import (
+	"2q2r/server"
+	"os"
+)
+
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	appID := "_T-zi0wzr7GCi4vsfsXsUuKOfmiWLiHBVbmJJPidvhA"
+
+	f, err := os.Open("./config.yaml")
+	handle(err)
+
+	config, err := server.MakeConfig(f, "yaml")
+	handle(err)
+
+	db := server.MakeDB(config)
+
+	err = db.Create(&server.AppInfo{
+		AppID:   appID,
+		AppName: "test-app",
+	}).Error
+	handle(err)
+
+	err = db.Create(&server.AppServerInfo{
+		ServerID: "_T-zi0wzr7GCi4vsfsXsUuKOfmiWLiHBVbmJJPidvhA",
+		BaseURL:  config.BaseURL,
+		AppID:    appID,
+	}).Error
+	handle(err)
+}
