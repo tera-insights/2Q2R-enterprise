f96f49cf4b1486194e024d0bb91c3b487486e28b
Alin Dobra <alin@terainsights.com>
2016-10-26 11:02:45 -0400

    Start work on filters for server

--- admin/src/controllers/ServersCtrl.ts
+++ admin/src/controllers/ServersCtrl.ts
@@ -5,10 +5,42 @@ import { Servers, IServerItem, IServerResource } from '../services/Servers';
 import { Apps, IAppItem, IAppResource } from '../services/Apps';
 import { DeleteServersCtrl } from './modals/DeleteServersCtrl';
 
+interface IServerExtendedItem extends IServerItem {
+    appName: string;
+}
+
+type filterAttType = 'serverName' | 'appName' | 'userCnt';
+
+
 export class ServersCtrl {
     private Server: IServerResource;
-    private servers: IServerItem[];
-    private selectedServers: IServerItem[] = [];
+    private App: IAppResource;
+    private servers: IServerExtendedItem[];
+    private apps: IAppItem[];
+
+    private appsByID: { [appID: string]: IAppItem } = {};
+
+    private selectedServers: IServerExtendedItem[] = [];
+    private filteredServers: IServerExtendedItem[] = [];
+
+    private filterAtt: filterAttType;
+    private filters: { [att: string]: any } = {
+        "serverName": {
+            type: "string",
+            name: "Server Name"
+        },
+        "appName": {
+            type: "string",
+            name: "Application Name"
+        },
+        "userCnt": {
+            type: "number",
+            name: "User Count"
+        }
+    }
+    private filterString: string = "";
+    private filterRange: { min: number, max: number };
+
     private orderBy: string;
     private state: 'searchClosed' | 'searchOpen' = 'searchClosed';
     private serverFilter: string;
@@ -19,7 +51,7 @@ export class ServersCtrl {
     }
 
     // Aux datascructures to organize Apps and Servers
-    private serversByAppID: { [appID: string] : IServerItem[] } = {};
+    private serversByAppID: { [appID: string]: IServerItem[] } = {};
 
     updateServer(server: IServerItem) {
 
@@ -39,25 +71,71 @@ export class ServersCtrl {
         });
     }
 
+    applyFilters() {
+        var filterFct: (IServerExtendedItem) => boolean;
+        var filterObj = this.filters[this.filterAtt];
+        if (filterObj) {
+            switch (filterObj.type) {
+                case "string":
+                    filterFct = (server: IServerExtendedItem): boolean => {
+                        return server[this.filterAtt].includes(this.filterString);
+                    };
+                    break;
+                case "number":
+                    filterFct = (server: IServerExtendedItem): boolean => {
+                        let val = server[this.filterAtt];
+                        return (val >= this.filterRange.min) &&
+                            (val <= this.filterRange.max);
+                    };
+                    break;
+
+                default:  // Cannot apply this, return true
+                    filterFct = (server: IServerExtendedItem): boolean => {
+                        return true;
+                    };
+            }
+            this.filteredServers = this.servers.filter(filterFct);
+        } else {
+            this.filteredServers = this.servers;
+        }
+    }
+
     // Get all the info from backend again
     refresh() {
         console.log("Refreshed servers!");
-        this.servers = this.Server.query();
-        // this.servers = this.Server.query( () => {
-        //     this.serversByAppID = _.groupBy(this.servers, 'appID');
-        // });
+        this.servers = <IServerExtendedItem[]>this.Server.query();
+        this.apps = this.App.query();
+
+        this.$q.all([
+            this.servers.$promise,
+            this.apps.$promise
+        ]).then(() => {
+            this.appsByID = _.keyBy(this.apps, 'appID');
+            this.servers.forEach((server: IServerExtendedItem) => {
+                var app = this.appsByID[server.appID];
+                if (app) {
+                    server.appName = app.appName;
+                }
+            });
+            this.applyFilters();
+        })
     }
 
     static $inject = [
+        '$q',
         '$mdDialog',
         '$mdToast',
+        'Apps',
         'Servers'
     ];
     constructor(
+        private $q: ng.IQService,
         private $mdDialog: ng.material.IDialogService,
         private $mdToast: ng.material.IToastService,
+        AppsSrvc: Apps,
         ServersSrvc: Servers
     ) {
+        this.App = AppsSrvc.resource;
         this.Server = ServersSrvc.resource;
         this.refresh();
     }
